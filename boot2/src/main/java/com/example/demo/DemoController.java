cb1f28f3d00fbe1abe8925132e2ccfef4f76e75b
sullis <github@seansullivan.com>
2025-09-27 16:25:56 -0700

    update DemoController.java

--- boot2/src/main/java/com/example/demo/DemoController.java
+++ boot2/src/main/java/com/example/demo/DemoController.java
@@ -7,12 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-  @PostMapping(consumes = "text/plain", produces = "text/plain")
+  @PostMapping(path = "/echo_plain_text",
+      consumes = "text/plain",
+      produces = "text/plain")
   public String echo_plain_text(@RequestBody String input) {
     return input;
   }
 
-  @PostMapping(consumes = "application/json", produces = "application/json")
+  @PostMapping(path = "/echo_json",
+      consumes = "application/json",
+      produces = "application/json")
   public String echo_json(@RequestBody String input) {
     return input;
   }
