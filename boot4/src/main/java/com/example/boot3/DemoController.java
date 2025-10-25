0c65f53d2788ad4a701057cdc7fcfb61d4a61e1f
sullis <github@seansullivan.com>
2025-10-25 15:45:40 -0700

    add Spring Boot 4 app

---
+++ boot4/src/main/java/com/example/boot3/DemoController.java
@@ -0,0 +1,28 @@
+package com.example.boot3;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DemoController {
+  @PostMapping(path = "/consumes_text_plain",
+      consumes = "text/plain",
+      produces = "text/plain")
+  public String consumes_text_plain(@RequestBody String input) {
+    return input;
+  }
+
+  @PostMapping(path = "/consumes_application_json",
+      consumes = "application/json",
+      produces = "application/json")
+  public String consumes_application_json(@RequestBody String input) {
+    return input;
+  }
+
+  @PostMapping(path = "/consumes_anything")
+  public String consumes_anything(@RequestBody String input) {
+    return input;
+  }
+}
---
+++ boot4/src/test/java/com/example/boot3/Boot3Tests.java
@@ -0,0 +1,18 @@
+package com.example.boot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import com.example.testlib.TestHelper;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class Boot3Tests {
+  @LocalServerPort int localServerPort;
+
+	@Test
+	void checkEndpoints() throws Exception {
+    TestHelper.checkEndpoints("http://localhost:" + localServerPort);
+	}
+
+}
